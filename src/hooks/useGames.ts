fb4827de9110b4c485dc5b5b46acd44f498dd70e
Awab Omer <awab228@yahoo.com>
2023-04-07 19:29:56 +0200

    Adds the GameCardSkeleton and modifies the components to use it

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,22 +23,27 @@ interface FetchGameGrid {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-
+    setLoading(true);
     apiClient
       .get<FetchGameGrid>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
