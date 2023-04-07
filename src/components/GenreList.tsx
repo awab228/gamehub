e369e4cfc28b67df91636874db4531778f7cb5e0
Awab Omer <awab228@yahoo.com>
2023-04-07 20:50:47 +0200

    Adds the genre background image, sets a fixed width for the sidebar 200px and fixes layout issues

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import UseGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = UseGenres();
   return (
-    <ul>
+    <List>
       {data.map((data) => (
-        <li key={data.id}>{data.name}</li>
+        <ListItem key={data.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="36px"
+              borderRadius={8}
+              src={getCroppedImageUrl(data.image_background)}
+            />
+            <Text fontSize="lg">{data.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
