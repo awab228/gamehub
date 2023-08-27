f20defa4dadede567e819764641d734fa4cc2f46
awab228 <awab228@yahoo.com>
2023-08-27 14:57:43 +0200

    changes the logic for fetching platforms to use react query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
 import platfroms from "../data/platfroms";
+import apiClient from "../services/api-client";
+import { FetchDataResponse } from "./useData";
+
 interface Platfrom {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platfroms, isLoadding: null, error: null });
+const usePlatforms = () => useQuery({
+  queryKey: ["platforms"],
+  queryFn: () =>
+    apiClient
+    .get<FetchDataResponse<Platfrom>>("platforms").then(res => res.data),
+  staleTime: 24 * 60 * 60 * 1000, //24h
+  initialData: { count: platfroms.length, results: platfroms}
+})
 
 export default usePlatforms;
