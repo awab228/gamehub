65a1e2820ebc2b116f2c84df341f7a61808d1a31
awab228 <awab228@yahoo.com>
2023-08-29 18:43:21 +0200

    adds staleTime to avoid hitting the backend too much

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -30,7 +30,8 @@ const useGames = (gameQuery: GameQuery) =>
         }),
         getNextPageParam: (lastPage, allPages) => {
           return lastPage.next ? allPages.length + 1 : undefined
-        }
+        },
+        staleTime: 24 * 60 * 60 * 100 , //24H
   })
 
 export default useGames;
