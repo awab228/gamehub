59d283e06c6a8706ddfe91711e289d6518b15eb4
awab228 <awab228@yahoo.com>
2023-08-30 22:04:09 +0200

    fix for github pages

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Link to={"/"}>
+      <Link to={"/gamehub"}>
         <Image src={logo} boxSize="60px" objectFit="cover" />
       </Link>
       <SearchInput />
